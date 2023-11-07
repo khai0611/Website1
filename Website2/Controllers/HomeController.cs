808f536129c7707a963ce098087559b6caa87792
hqk0611 <khaihq19@uef.edu.vn>
2023-11-07 17:35:10 +0700

    Add project files.

---
+++ Website2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult TrangChu()
+        {
+            //redirect to another action, have common controller
+            //return RedirectToAction("DangNhap");
+
+            //redirect to different controller
+            //return RedirectToAction("DanhSachTinTuc", "TinTuc");
+
+            //redirect to URL
+            //return Redirect("https://google.com");
+
+            return View();
+        }
+
+        public ActionResult GioiThieu()
+        {
+            return View();
+        }
+
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+    }
+}
---
+++ Website2/Controllers/TinTucController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website2.Controllers
+{
+    public class TinTucController : Controller
+    {
+        // GET: TinTuc
+        public ActionResult DanhSachTinTuc()
+        {
+            return View();
+        }
+
+        public ActionResult NoiDungTinTuc()
+        {
+            return View();
+        }
+    }
+}
